1552889c0dd8e951bbcb0c5bea7935881e6cc87d
马志强 <172697808@qq.com>
2017-12-25 19:21:05 +0800

    Initial Commit

---
+++ SDYIphoneHotel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDYIphoneHotel
+//
+//  Created by admin on 2017/12/25.
+//  Copyright © 2017年 SanDaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDYIphoneHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDYIphoneHotel
+//
+//  Created by admin on 2017/12/25.
+//  Copyright © 2017年 SanDaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
